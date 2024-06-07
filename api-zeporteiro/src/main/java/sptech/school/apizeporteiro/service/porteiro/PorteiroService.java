ca413c9b5f3c2cf786cf3aec23cc0c7494c03656
eusoubrian <brian.gomes@sptech.school>
2024-06-07 15:37:01 -0300

    feat(matriz): Adicionando matriz no projeto na porteiro service

--- api-zeporteiro/src/main/java/sptech/school/apizeporteiro/service/porteiro/PorteiroService.java
+++ api-zeporteiro/src/main/java/sptech/school/apizeporteiro/service/porteiro/PorteiroService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import sptech.school.apizeporteiro.domain.condominio.Condominio;
 import sptech.school.apizeporteiro.domain.condominio.repository.CondominioRepository;
+import sptech.school.apizeporteiro.domain.entrega.Entrega;
 import sptech.school.apizeporteiro.domain.entrega.repository.EntregaRepository;
 import sptech.school.apizeporteiro.domain.porteiro.Porteiro;
 import sptech.school.apizeporteiro.domain.porteiro.repository.PorteiroRepository;
@@ -39,6 +40,10 @@ public class PorteiroService {
 
     public List<PorteiroListagemDto> listarPorCondominio(Integer condominioId) {
         List<Porteiro> porteiros = porteiroRepository.findByCondominioId(condominioId);
+
+        // Chama a lógica de matriz e imprime a matriz de porteiros e entregas
+        matrizPorteirosEntregas(porteiros);
+
         return porteiros.stream()
                 .map(PorteiroMapper::toDto)
                 .collect(Collectors.toList());
@@ -74,5 +79,43 @@ public class PorteiroService {
 
         return PorteiroMapper.toEntregaDto(porteiro.getEntregas());
     }
+
+    // Adicionando a lógica de matriz para porteiros e suas entregas
+    public void matrizPorteirosEntregas(List<Porteiro> porteiros) {
+        int numPorteiros = porteiros.size();
+
+        int numTiposEntregas = 3;
+        int[][] matriz = new int[numPorteiros][numTiposEntregas];
+
+        for (int i = 0; i < numPorteiros; i++) {
+            Porteiro porteiro = porteiros.get(i);
+            List<Entrega> entregas = porteiro.getEntregas();
+            int tipo1 = 0, tipo2 = 0, tipo3 = 0;
+            for (Entrega entrega : entregas) {
+                if (entrega.getTipoEntrega().equals("Tipo1")) tipo1++;
+                if (entrega.getTipoEntrega().equals("Tipo2")) tipo2++;
+                if (entrega.getTipoEntrega().equals("Tipo3")) tipo3++;
+            }
+            matriz[i][0] = tipo1;
+            matriz[i][1] = tipo2;
+            matriz[i][2] = tipo3;
+        }
+
+        // Exibindo a matriz
+        exibirMatrizPorteirosEntregas(matriz, porteiros);
+    }
+
+    private void exibirMatrizPorteirosEntregas(int[][] matriz, List<Porteiro> porteiros) {
+        System.out.println("Matriz de Porteiros e suas Entregas:");
+        System.out.println("Porteiro | Tipo1 | Tipo2 | Tipo3");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(porteiros.get(i).getNome() + " | ");
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
 }
+
 
