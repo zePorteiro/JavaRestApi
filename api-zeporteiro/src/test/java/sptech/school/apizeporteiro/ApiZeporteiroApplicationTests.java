04e28ae34ac93223804ce2860a7e282333f18bc7
Tiaki Wauke Cardozo <125319667+TiakiWCardozo@users.noreply.github.com>
2024-12-03 19:30:49 -0300

    Update ApiZeporteiroApplicationTests.java

--- api-zeporteiro/src/test/java/sptech/school/apizeporteiro/ApiZeporteiroApplicationTests.java
+++ api-zeporteiro/src/test/java/sptech/school/apizeporteiro/ApiZeporteiroApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiZeporteiroApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+public class ApiZeporteiroApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
 
 }
